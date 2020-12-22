6a6957e6a40a2a988f7bd1b90c87994582c7f68a
daniel.kohlsdorf <dkohlsdorf@gmail.com>
2020-12-22 16:54:45 +0100

    adding euclidean distance and 1 dimensional dtw

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+#[macro_use]
+
+pub mod numerics;
+
 fn main() {
     println!("Hello, world!");
+    let x = vec![0.1,0.2,0.3,0.4];
+    let y = vec![0.2,0.2,0.3,0.4];
+    println!("{}", numerics::euclidean(&x, &y));
+    println!("{}", numerics::dtw1d(&x, &y, 0));
 }
