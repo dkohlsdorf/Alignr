a1e50bb4b982c42f73b44a88d12783111edc3c8f
daniel.kohlsdorf <dkohlsdorf@gmail.com>
2020-12-22 17:13:40 +0100

    hamming windows

--- src/main.rs
+++ src/main.rs
@@ -8,4 +8,5 @@ fn main() {
     let y = vec![0.2,0.2,0.3,0.4];
     println!("{}", numerics::euclidean(&x, &y));
     println!("{}", numerics::dtw1d(&x, &y, 0));
+    println!("{:?}", numerics::hamming(11));
 }
